ae787136e72ecd8ceed5ac960a73f3bd33b0aa77
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-07 17:32:36 -0300

    Rota GET /users finalizada

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,20 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import mongoose from 'mongoose';
-
 import { Users } from '../../models.js'
 
 const db = mongoose.connection;
 
-export default async (req, res) => {
+export default (req, res) => {
 
-  await mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-  db.on('error', console.error.bind(console, 'connection error:'));
+  mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .catch(error => console.log(error));
 
-  await Users.findOne({ gender: "male" }, function (err, doc) {
+  Users.find(function (err, docs) {
     if (err) return console.error(err);
-    console.log(doc)
+    res.status(200).json(docs)
   })
-
-  res.status(200).json({ name: 'John Doe' })
 
 }
