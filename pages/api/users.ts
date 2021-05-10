561b0de94dd62ddfff149274f531266b0d2657b8
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-10 12:26:57 -0300

    Rotas GET /users/:userId e DELETE /users/:userId finalizadas e arquivo customTypes.ts criado

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,17 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import mongoose from 'mongoose';
 import { Users } from '../../models.js'
+import { User } from '../../customTypes.js';
 
 mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .catch(error => console.log(error));
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
 
   mongoose.connection.on('error', err => {
     console.log(err);
   });
 
-  await Users.find(function (err, docs) {
+  Users.find(function (err, docs:Array<User>) {
     if (err) return console.error(err);
     res.status(200).json(docs)
   })
---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mongoose from 'mongoose';
+import { Users } from '../../../models.js'
+import { User } from '../../../customTypes.js';
+
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .catch(error => console.log(error));
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+
+  const { userId } = req.query;
+
+  mongoose.connection.on('error', err => {
+    console.log(err);
+  });
+
+  switch (req.method) {
+    case 'GET':
+      Users.findOne({ 'login.uuid': userId }, function (err, doc: User) {
+        if (err) return console.error(err);
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.status(404).send('Usuário não encontrado.');
+        }
+      })
+      break;
+
+    case 'DELETE':
+      Users.findOneAndDelete({ 'login.uuid': userId }, null, function (err, doc) {
+        if (err) return console.error(err);
+        if (doc) {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.status(200).send('Usuário com uuid: ' + (doc as unknown as User).login.uuid + ' excluído com sucesso.');
+        } else {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.status(404).send('Usuário não encontrado');
+        }
+      })
+      break;
+
+
+    default: break;
+  }
+
+}
