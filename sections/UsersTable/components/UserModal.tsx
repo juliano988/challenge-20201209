8a6f85f61caf9694d78bd00c2915e476db91e112
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-11 15:57:57 -0300

    Modal acrescido da funcionalidade de exclusão.

--- sections/UsersTable/components/UserModal.tsx
+++ sections/UsersTable/components/UserModal.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../../../styles/sections/UsersTable/UserModal-styles.module.scss';
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Spinner } from "react-bootstrap";
 import { TableItem } from "../../../customTypes";
 import ReactCountryFlag from "react-country-flag"
 import { CircleFlag } from 'react-circle-flags'
 
 
 export default function UserModal(props: { modalUserData: TableItem, showUserModal: boolean, setShowUserModal: React.Dispatch<React.SetStateAction<boolean>> }) {
+
+  const [deleteBtnClickCount, setdeleteBtnClickCount] = useState<number>(0);
+
+  function handleOnHide() {
+    setdeleteBtnClickCount(0);
+    props.setShowUserModal(false);
+  }
+
+  async function handleDeleteBtn() {
+    setdeleteBtnClickCount(deleteBtnClickCount + 1);
+    if (deleteBtnClickCount + 1 === 2) {
+      await fetch('/api/users/' + props.modalUserData.id, { method: "DELETE" });
+      handleOnHide();
+    }
+  }
+
   return (
-    <Modal centered show={props.showUserModal} onHide={() => props.setShowUserModal(false)}>
+    <Modal centered show={props.showUserModal} onHide={handleOnHide}>
       <Modal.Body>
         <div className={styles.user_img}>
           {props.modalUserData?.image}
@@ -25,9 +41,14 @@ export default function UserModal(props: { modalUserData: TableItem, showUserMod
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide} variant="warning">Atualizar</Button>
-        <Button onClick={props.onHide} variant="danger">Excluir</Button>
+        {/* <Button onClick={props.onHide} variant="warning">Atualizar</Button> */}
+        <Button onClick={handleDeleteBtn}
+          variant="danger"
+          disabled={deleteBtnClickCount === 2 ? true : false}>
+          {deleteBtnClickCount === 0 ? 'Excluir' : deleteBtnClickCount === 1 ? 'Tem certeza?' :
+            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />}
+        </Button>
       </Modal.Footer>
-    </Modal>
+    </Modal >
   )
 }
